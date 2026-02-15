14a5de80ad143890b5863c7fcd29105cba3f948e
lucaslatessa <lucaslatessa@gmail.com>
2026-02-15 15:05:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def enviar_mensaje(to, text):
     return response.json()
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
